e96c48380c583926fb4d50833740a63ae342d152
Lorenzo Tucci <yamabiiko@protonmail.com>
2024-08-13 14:52:51 +0300

    docs: add crate documentation and update README

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# lhtlp
-An implementation of Linearly Homorphic TimeLock Puzzles (LHTLP) in Rust
+# Linearly Homorphic TimeLock Puzzles (LHTLP) Implementation in Rust
+
+This LHTLP implementation is written purely in Rust, building on top of `num-bigint` and `num-primes` crates.
+
+It implements the protocol described in Section 4.1 of [Homomorphic Time-Lock Puzzles and Applications](https://eprint.iacr.org/2019/635.pdf), Malavolta and Thyagarajan, 2019.
+
+## What is a LHTLP?
+
+A LHTLP is a linearly homomorphic time-lock puzzles. Time-lock puzzles are cryptographic primitives that allow to encrypt a secret in a puzzle that can only be recovered after performing a certain amount of operations that are inherently sequential (squaring in RSA groups). The linearly homorphic properties imply that a set of puzzles can be evaluated homomorphically, that is a set of puzzles of can be bundled up together or evaluated with a circuit as a single puzzle. See [https://eprint.iacr.org/2019/635.pdf](https://eprint.iacr.org/2019/635.pdf) for more details.
+
+## Usage
+### Setup, generate and solve a puzzle
+Setting up a LHTLP requires 2 parameters:
+* _lambda_: security parameters that sets the number of bits of the randomly generated safe primes
+* _difficulty_: number of iterations to perform, linearly increasing computation time when retrieving the secret with `solve`
+```rust
+ use lhltp::LHTLP;
+ const difficulty: u64 = 100000000;
+ const lambda: u64 = 64;
+
+ let lhtlp = LHTLP::setup(lambda, BigUint::from(difficulty));
+ let secret = 42;
+ let puzzle = lhtlp.generate(secret);
+ let solution = lhtlp:solve(puzzle);
+```
+### Homomorphic evaluation of multiple puzzles
+```rust
+ let first = lhtlp.generate(42);
+ let second = lhtlp.generate(13);
+ let bundle = lhtlp.eval(vec![first, second]);
+ let solution = lhtlp:solve(puzzle);
+
+ assert!(BigUint::from(55u32), solution);
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,51 @@
+//! # lhtlp -  Linearly Homomorphic TimeLock Puzzles (LHTLP) implementation
+//! This crate provides a simple library implementation of LHTLP in pure Rust.
+//! ## Setup, generate and solve a puzzle
+//! ```rust
+//! use lhltp::LHTLP;
+//! const TIME_HARDNESS: u64 = 100000000;
+//!
+//! let lhtlp = LHTLP::setup(64, BigUint::from(TIME_HARDNESS));
+//! let secret = 42;
+//! let puzzle = lhtlp.generate(secret);
+//! let solution = lhtlp:solve(puzzle);
+//! ```
+//! ## Homomorphic evaluation of multiple puzzles
+//! ```rust
+//! let first = lhtlp.generate(42);
+//! let second = lhtlp.generate(13);
+//! let bundle = lhtlp.eval(vec![first, second]);
+//! let solution = lhtlp:solve(puzzle);
+//!
+//! assert!(BigUint::from(55u32), solution);
+//! ```
 use num_primes::{Generator, RandBigInt};
 use num_bigint::BigUint;
 use num_traits::pow::Pow;
 use num_integer::Integer;
 
-#[derive(Debug)]
+/// A Linearly Homomorphic Timelock Puzzle.
+///
+/// A LHTLP is a linearly homomorphic version of time-lock puzzles, which are cryptographic primitives that
+/// allow to encrypt a secret in a puzzle that can only be recovered after performing a certain
+/// amount of sequential operations.
+#[derive(Debug, Clone)]
 pub struct LHTLP {
-    time_hardness: BigUint,
+    difficulty: BigUint,
     n: BigUint,
     g: BigUint,
     h: BigUint
 }
 
 impl LHTLP {
-    pub fn setup(time_hardness: BigUint) -> LHTLP { 
-        let p = Generator::safe_prime(64);
-        let q = Generator::safe_prime(64);
+    /// Setup an instance of a LHTLP based on time and security parameter.
+    ///
+    /// The security parameter `lambda` sets the number of bits of the randomly generated safe primes. \
+    /// Setting `difficulty` to 100000000 results in roughly 5 seconds of computation when
+    /// opening a puzzle with `solve`.
+    pub fn setup(lambda: u64, difficulty: BigUint) -> LHTLP {
+        let p = Generator::safe_prime(lambda);
+        let q = Generator::safe_prime(lambda);
 
         let n = &p * &q;
         let one = BigUint::from(1u32);
@@ -34,17 +65,19 @@ impl LHTLP {
         // phi(n) = p-1 * q-1
         // phi(n) / 2
         let tot_div_2 = (&p-&one) * (&q-&one) / &BigUint::from(2u32);
-        let pow = &two.modpow(&time_hardness, &tot_div_2); 
+        let pow = &two.modpow(&difficulty, &tot_div_2);
         let h = g.modpow(pow, &n);
 
         LHTLP { 
-            time_hardness, 
+            difficulty,
             n,
             g,
             h,
         }
     }
 
+    /// Generate a puzzle `(u: BigUint, v: BigUint)` embedding a `secret` value.
+    ///
     pub fn generate(&self, secret: u64) -> (BigUint, BigUint) {
         let mut rng = rand::thread_rng();
         let n2 = (&self.n).pow(2u32);
@@ -52,21 +85,24 @@ impl LHTLP {
         let r = rng.gen_biguint_range(&one, &n2);
         let u = self.g.modpow(&r, &self.n);
         let v = ((&self.h).modpow(&(&r * &self.n), &n2) * (&one + &self.n).modpow(&BigUint::from(secret), &n2)) % n2;
-        //println!("u, v {}, {}", u, v);
         (u, v)
     }
 
+    /// Open a puzzle `(u: BigUint, v: BigUint)` by performing sequential squaring, revealing a `secret` value.
+    ///
     pub fn solve(&self, puzzle: (BigUint, BigUint)) -> BigUint {
         let n2 = (&self.n).pow(2u32);
-        let w = puzzle.0.modpow(&BigUint::from(2u32).pow(&self.time_hardness), &self.n);
+        let w = puzzle.0.modpow(&BigUint::from(2u32).pow(&self.difficulty), &self.n);
         let s = ((&puzzle.1 * w.modpow(&self.n, &n2).modinv(&n2).unwrap()) % (&self.n).pow(2u32) -  BigUint::from(1u32))/ &self.n;
         s
     }
-    pub fn evaluate(&self, puzzles: Vec<(BigUint, BigUint)>) -> (BigUint, BigUint) {
 
+    /// Linearly homomorphic evaluate a vector of puzzles.
+    ///
+    /// The resulting puzzle embeds a secret equivalent to the sum of the secrets embedded in the single puzzles.
+    pub fn evaluate(&self, puzzles: Vec<(BigUint, BigUint)>) -> (BigUint, BigUint) {
         let one = BigUint::from(1u32);
-        let (u, v): (BigUint, BigUint) = puzzles.iter().fold((one.clone(), one), |acc, x| ((acc.0 * &x.0), (acc.1 * &x.1)));
-        (u, v)
+        puzzles.iter().fold((one.clone(), one), |acc, x| ((acc.0 * &x.0), (acc.1 * &x.1)))
     }
 }
 
@@ -76,13 +112,14 @@ mod tests {
     use rand::Rng;
 
     // Roughly 5 sec, increases linearly
-    const TIME_HARDNESS: u64 = 100000000;
+    const DIFFICULTY: u64 = 100000000;
+    const LAMBDA: u64 = 64;
     
     #[test]
     fn gen_and_solve() {
         let mut rng = rand::thread_rng();
         let secret: u64 = rng.gen();
-        let lhtlp = LHTLP::setup(BigUint::from(TIME_HARDNESS));
+        let lhtlp = LHTLP::setup(LAMBDA, BigUint::from(DIFFICULTY));
         let puzzle = lhtlp.generate(secret);
         let result = lhtlp.solve(puzzle);
         assert!(BigUint::from(secret) == result);
@@ -90,7 +127,7 @@ mod tests {
 
     #[test]
     fn evaluate() {
-        let lhtlp = LHTLP::setup(BigUint::from(TIME_HARDNESS));
+        let lhtlp = LHTLP::setup(LAMBDA, BigUint::from(DIFFICULTY));
         let mut rng = rand::thread_rng();
         let mut secrets: Vec<u64> = Vec::new();
         let mut puzzles: Vec<(BigUint, BigUint)> = Vec::new();
